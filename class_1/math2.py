e3b6f640f29a9f4406a43df58e8865d2754cdee5
Melanie Beaulieu <77403345+mbeaulieu-java@users.noreply.github.com>
2021-11-04 10:38:03 -0400

    Delete class_1 directory

--- class_1/math2.py
+++
@@ -1,9 +0,0 @@
-number_diners = 3
-number_diners = number_diners + 1
-number_diners += 1
-number_diners -= 2
-
-# number_diners++
-
-print(number_diners)
-
--- class_1/strings2.py
+++
@@ -1,9 +0,0 @@
-string_1 = "Hello"
-string_2 = "World"
-
-print(string_1, end="")
-print(string_2)
-
-
-print("*" * 20)
-
