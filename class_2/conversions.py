2cffe504b79eb0f3ba7c116ac4d4bbd4767584c4
Melanie Beaulieu <77403345+mbeaulieu-java@users.noreply.github.com>
2021-11-04 10:38:24 -0400

    Delete class_2 directory

--- class_2/conversions.py
+++
@@ -1,6 +0,0 @@
-age = "22"
-bigger_age = int(age) + 2
-print("Your age plus 2 years is " + str(bigger_age))
-
-print("Your age plus 2 years is {}".format(bigger_age))
-
--- class_2/debug_example.py
+++
@@ -1,6 +0,0 @@
-age = int(input("what is your age"))
-
-a = 4
-
-b = 5
-
